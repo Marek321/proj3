976155735f9fcc21c5e68591b9b85287a2589a33
Martin Očenáš <martin@debian.Home>
2013-04-17 13:35:38 +0200

    První verze grafického rozhraní

---
+++ mainwindow.cpp
@@ -0,0 +1,153 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QKeyEvent>
+
+GSCalculator::GSCalculator(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::GSCalculator)
+{
+  ui->setupUi(this);
+  connect(ui->ukoncit,SIGNAL(triggered()),this,SLOT(quit()));
+  connect(ui->Button_clear,SIGNAL(pressed()),this,SLOT(Button_clear()));
+  connect(ui->Button_0,SIGNAL(pressed()),this,SLOT(Button_0()));
+  connect(ui->Button_1,SIGNAL(pressed()),this,SLOT(Button_1()));
+  connect(ui->Button_2,SIGNAL(pressed()),this,SLOT(Button_2()));
+  connect(ui->Button_3,SIGNAL(pressed()),this,SLOT(Button_3()));
+  connect(ui->Button_4,SIGNAL(pressed()),this,SLOT(Button_4()));
+  connect(ui->Button_5,SIGNAL(pressed()),this,SLOT(Button_5()));
+  connect(ui->Button_6,SIGNAL(pressed()),this,SLOT(Button_6()));
+  connect(ui->Button_7,SIGNAL(pressed()),this,SLOT(Button_7()));
+  connect(ui->Button_8,SIGNAL(pressed()),this,SLOT(Button_8()));
+  connect(ui->Button_9,SIGNAL(pressed()),this,SLOT(Button_9()));
+  connect(ui->Button_plus,SIGNAL(pressed()),this,SLOT(Button_plus()));
+  connect(ui->Button_minus,SIGNAL(pressed()),this,SLOT(Button_minus()));
+  connect(ui->Button_divide,SIGNAL(pressed()),this,SLOT(Button_divide()));
+  connect(ui->Button_multiply,SIGNAL(pressed()),this,SLOT(Button_multiply()));
+  connect(ui->Button_power,SIGNAL(pressed()),this,SLOT(Button_power()));
+  connect(ui->Button_factorial,SIGNAL(pressed()),this,SLOT(Button_factorial()));
+  connect(ui->Button_modulo,SIGNAL(pressed()),this,SLOT(Button_modulo()));
+  connect(ui->Button_comma,SIGNAL(pressed()),this,SLOT(Button_comma()));
+}
+
+GSCalculator::~GSCalculator()
+{
+  delete ui;
+}
+
+void GSCalculator::quit()
+{
+  QApplication::quit();
+}
+
+void GSCalculator::Button_handle(char znak)
+{
+  this->displayed_string+=znak;
+  this->display();
+}
+
+void GSCalculator::display()
+{
+  ui->Display->setText(QString(this->displayed_string.c_str()));
+}
+
+/*void Button_solve()
+{
+  this->displayed_string=eval(this->displayed_string);
+  this->display();
+}*/
+
+void GSCalculator::Button_clear()
+{
+  this->displayed_string.clear();
+  this->display();
+}
+
+void GSCalculator::Button_0()
+{
+  Button_handle('0');
+}
+
+void GSCalculator::Button_1()
+{
+  Button_handle('1');
+}
+
+void GSCalculator::Button_2()
+{
+  Button_handle('2');
+}
+
+void GSCalculator::Button_3()
+{
+  Button_handle('3');
+}
+
+void GSCalculator::Button_4()
+{
+  Button_handle('4');
+}
+
+void GSCalculator::Button_5()
+{
+  Button_handle('5');
+}
+
+void GSCalculator::Button_6()
+{
+  Button_handle('6');
+}
+
+void GSCalculator::Button_7()
+{
+  Button_handle('7');
+}
+
+void GSCalculator::Button_8()
+{
+  Button_handle('8');
+}
+
+void GSCalculator::Button_9()
+{
+  Button_handle('9');
+}
+
+void GSCalculator::Button_plus()
+{
+  Button_handle('+');
+}
+
+void GSCalculator::Button_minus()
+{
+  Button_handle('-');
+}
+
+void GSCalculator::Button_divide()
+{
+  Button_handle('/');
+}
+
+void GSCalculator::Button_multiply()
+{
+  Button_handle('*');
+}
+
+void GSCalculator::Button_power()
+{
+  Button_handle('^');
+}
+
+void GSCalculator::Button_factorial()
+{
+  Button_handle('!');
+}
+
+void GSCalculator::Button_modulo()
+{
+  Button_handle('%');
+}
+
+void GSCalculator::Button_comma()
+{
+  Button_handle('.');
+}
---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <string.h>
+
+namespace Ui {
+  class GSCalculator;
+}
+
+class GSCalculator : public QMainWindow
+{
+  Q_OBJECT
+  
+public:
+  explicit GSCalculator(QWidget *parent = 0);
+  ~GSCalculator();
+  std::string displayed_string;
+  
+private:
+  Ui::GSCalculator *ui;
+
+private slots:
+  void quit();
+  void display();
+  void Button_handle(char znak);
+  //void Button_solve();
+  void Button_0();
+  void Button_1();
+  void Button_2();
+  void Button_3();
+  void Button_4();
+  void Button_5();
+  void Button_6();
+  void Button_7();
+  void Button_8();
+  void Button_9();
+  void Button_clear();
+  void Button_plus();
+  void Button_minus();
+  void Button_divide();
+  void Button_multiply();
+  void Button_power();
+  void Button_factorial();
+  void Button_modulo();
+  void Button_comma();
+};
+
+#endif // MAINWINDOW_H
